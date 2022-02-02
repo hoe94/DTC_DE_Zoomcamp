f5c6ce449d2c7e353cf798f35aaa26605d3340d2
Low Kim Hoe <lkh_bear94@hotmail.com>
2022-02-02 15:50:44 +0800

    Add files via upload

---
+++ Homework/Week_2/data_ingestion_local.py
@@ -0,0 +1,62 @@
+import os
+from datetime import datetime
+
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+from ingest_script import ingest_callable
+
+AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+
+PG_HOST = os.getenv('PG_HOST')
+PG_USER = os.getenv('PG_USER')
+PG_PASSWORD = os.getenv('PG_PASSWORD')
+PG_PORT = os.getenv('PG_PORT')
+PG_DATABASE = os.getenv('PG_DATABSE')
+
+default_args = {
+    "owner": "airflow",
+    "start_date": days_ago(1),
+    #"start_date": datetime(2021,1,1),
+    "depends_on_past": False,
+    "retries": 1,
+}
+
+#url = 'https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-01.csv'
+URL_PREFIX = 'https://s3.amazonaws.com/nyc-tlc/trip+data' 
+URL_TEMPLATE = URL_PREFIX + '/yellow_tripdata_{{ execution_date.strftime(\'%Y-%m\') }}.csv'
+OUTPUT_FILE_TEMPLATE = AIRFLOW_HOME + '/output_{{ execution_date.strftime(\'%Y-%m\') }}.csv'
+TABLE_NAME_TEMPLATE = 'yellow_taxi_trips_{{ execution_date.strftime(\'%Y_%m\') }}'
+
+with DAG(
+    dag_id = "LocalIngestionDag",
+    schedule_interval = '@monthly',
+    default_args = default_args,
+    tags = ['dtc-de']
+    
+
+) as dag:
+
+    wget_task = BashOperator(
+        task_id = 'wget',
+        bash_command= f'curl --sSL {URL_TEMPLATE} > {OUTPUT_FILE_TEMPLATE}'
+    )
+    
+    ingest_task = PythonOperator(
+        task_id = 'ingest',
+        python_callable = ingest_callable,
+        op_kwargs = dict(
+            #user = PG_USER,
+            #password = PG_PASSWORD,
+            #host = PG_HOST,
+            #port = PG_PORT,
+            #db = PG_DATABASE,
+            table_name = TABLE_NAME_TEMPLATE,
+            csv_file = OUTPUT_FILE_TEMPLATE
+        )
+    )
+    
+    wget_task >> ingest_task
+    
